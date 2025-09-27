9c8fe1a9d86e6787fd8baea14df703c516d3609f
dany <cursosverano0@gmail.com>
2025-09-27 13:23:31 +0200

    Inicializar repository

--- lib/initializer/initializeApi.dart
+++ lib/initializer/initializeApi.dart
@@ -1,17 +1,26 @@
 import 'package:logger/logger.dart';
 import 'package:get/get.dart';
 import 'package:rate_my_match_v2/data/providers/api/api_client.dart';
+
+import '../data/repositories/contracts/event_repository.dart';
+import '../data/repositories/implementations/event_repository_remote.dart';
+
 ///
-class InitializeApi{
+class InitializeApi {
   ///
   static final Logger _logg = Logger();
-  
-  static Future<void> initServices() async{
+
+  static Future<void> initServices() async {
     _logg.i('Inicializando servicios...');
 
     ///ApiClient
-    await Get.putAsync<ApiClient>(() => ApiClient(logger: _logg).init(), permanent: true);
+    await Get.putAsync<ApiClient>(
+      () => ApiClient(logger: _logg).init(),
+      permanent: true,
+    );
 
-
+    ///Event Repository
+    /// fenix: true asegura que si se elimina de la memoria, se recreará en el próximo Get.find()
+    Get.lazyPut<EventRepository>(() => EventRepositoryRemote(), fenix: true);
   }
 }
